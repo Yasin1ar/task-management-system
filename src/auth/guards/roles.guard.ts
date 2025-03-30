becc1acb7859271a404bc0c02455aad74179d748
yasin1ar <yasin.amirany@gmail.com>
2025-03-30 04:30:11 +0330

    refactor roles guard  to use auth  interface

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,10 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UserRole } from '@prisma/client';
+import { RequestWithUser } from '../interfaces/auth.interfaces';
 
 /**
  * Roles Guard
- * 
+ *
  * This guard is used to protect routes based on user roles.
  * It checks if the authenticated user has the required role to access the route.
  */
@@ -14,10 +15,10 @@ export class RolesGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     // Get the roles required for the route
-    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>('roles', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      'roles',
+      [context.getHandler(), context.getClass()],
+    );
 
     // If no roles are required, allow access
     if (!requiredRoles) {
@@ -25,9 +26,14 @@ export class RolesGuard implements CanActivate {
     }
 
     // Get the user from the request
-    const { user } = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+
+    // If no user is present in the request, deny access
+    if (!request.user) {
+      return false;
+    }
 
     // Check if the user has the required role
-    return requiredRoles.some((role) => user.role === role);
+    return requiredRoles.some((role) => request.user.role === role);
   }
 }
