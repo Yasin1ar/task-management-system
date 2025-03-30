f554ba3cd280b78a28c363d55d13531ccdb8a287
yasin1ar <yasin.amirany@gmail.com>
2025-03-30 04:33:48 +0330

    refactor current user decorator to use auth  interface

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,14 +1,18 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  AuthenticatedUser,
+  RequestWithUser,
+} from '../interfaces/auth.interfaces';
 
 /**
  * Current User Decorator
- * 
+ *
  * This decorator extracts the user from the request object.
  * It can be used in controller methods to get the authenticated user.
  */
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+  (data: unknown, ctx: ExecutionContext): AuthenticatedUser => {
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
     return request.user;
   },
 );
