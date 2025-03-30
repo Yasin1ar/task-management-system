c43c0614badfe51bd6287949316fc3ed810d2dbc
yasin1ar <yasin.amirany@gmail.com>
2025-03-30 17:14:49 +0330

    update user mod

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,18 @@
+/**
+ * Users Module
+ *
+ * This module handles user management functionality.
+ * It provides admin-only endpoints for managing users.
+ */
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
